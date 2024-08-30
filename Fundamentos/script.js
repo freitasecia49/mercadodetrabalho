78591d80f8f34ec4eb718462ce17fbe8b2320831
freitasecia49 <freitasecia49@gmail.com>
2024-08-29 21:32:15 -0300

    Add files via upload

---
+++ Fundamentos/script.js
@@ -0,0 +1,8 @@
+console.log("Olá, Mundo!");
+let preco = prompt ("Digite um preço");
+document.write(preco);
+
+let nome = prompt("Digite seu nome");
+document.write("Olá ",nome, " Seja bem vindo!");
+
+//olá, fulana.
