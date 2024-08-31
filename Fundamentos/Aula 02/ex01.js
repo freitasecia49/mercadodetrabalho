0107ed64c1758bc4e8a73d85275e17ed30b4e38c
freitasecia49 <freitasecia49@gmail.com>
2024-08-30 21:28:35 -0300

    Add files via upload

---
+++ Fundamentos/Aula 02/ex01.js
@@ -0,0 +1,14 @@
+/*Declare duas variáveis chamadas "idade" e "peso",
+atribua valores a elas e imprima os valores no console*/
+
+let idade = 46;
+let peso = 72;
+//let nome = "Fernando";
+//let prof = "Thais";
+
+console.log("Olá, você tem "+idade+" anos e o seu peso é de "+peso+"kg");
+
+//console.log("Nome da professora", prof, );
+//console.log("e Aluno", nome, );
+//console.log("idade=", idade);
+//console.log("de peso=", peso);
