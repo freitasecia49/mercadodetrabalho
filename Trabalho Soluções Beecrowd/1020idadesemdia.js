1293e5c6ed8726ca34d30b456a7dfcdcd3f34b8a
freitasecia49 <freitasecia49@gmail.com>
2024-09-19 17:13:26 -0300

    Add files via upload

---
+++ Trabalho Soluções Beecrowd/1020idadesemdia.js
@@ -0,0 +1,32 @@
+var idade;
+var ano = 365
+var mes = 30
+var dias;
+
+
+idade = 400;
+
+x = idade / ano;
+y = (idade % ano)/mes;
+dias = (idade % ano)-mes
+console.log(x.toFixed(0), "ano(s)");
+console.log(y.toFixed(0), "mes(s)");
+console.log(dias.toFixed(0), "dia(s)");
+
+idade = 800;
+
+x = idade / ano;
+y = (idade % ano)/mes;
+dias = (idade % ano)-mes
+console.log(x.toFixed(0), "ano(s)");
+console.log(y.toFixed(0), "mes(s)");
+console.log(dias.toFixed(0), "dia(s)");
+
+idade = 30;
+
+x = idade / ano;
+y = (idade % ano)/mes;
+dias = (idade % ano)-mes
+console.log(x.toFixed(0), "ano(s)");
+console.log(y.toFixed(0), "mes(s)");
+console.log(dias.toFixed(0), "dia(s)");
